8775bb16cbcfc01337c6f5b11f3b25943641793b
GibberishRacoon <frugolekes@gmail.com>
2024-10-23 16:36:04 +0200

    test comp. with header

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import React, { useState } from 'react';
-import ProjectFeed from './components/ProjectFeed/ProjectFeed';
-import ProjectView from './components/ProjectView/ProjectView';
+import React, { useState } from "react";
+import ProjectFeed from "./components/ProjectFeed/ProjectFeed";
+import ProjectView from "./components/ProjectView/ProjectView";
+import Hedaer from "./components/Header/Hearder";
 
 const App = () => {
-    const [selectedProject, setSelectedProject] = useState(null);
+  const [selectedProject, setSelectedProject] = useState(null);
 
-    const handleProjectClick = (project) => {
-        setSelectedProject(project);
-    };
+  const handleProjectClick = (project) => {
+    setSelectedProject(project);
+  };
 
-    const handleCloseView = () => {
-        setSelectedProject(null);
-    };
+  const handleCloseView = () => {
+    setSelectedProject(null);
+  };
 
-    return (
-        <div>
-            {!selectedProject ? (
-                <ProjectFeed onProjectClick={handleProjectClick} />
-            ) : (
-                <ProjectView project={selectedProject} onClose={handleCloseView} />
-            )}
-        </div>
-    );
+  return (
+    <>
+      <Hedaer />
+      <div>
+        {!selectedProject ? (
+          <ProjectFeed onProjectClick={handleProjectClick} />
+        ) : (
+          <ProjectView project={selectedProject} onClose={handleCloseView} />
+        )}
+      </div>
+    </>
+  );
 };
 
 export default App;
