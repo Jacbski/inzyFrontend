6be9fe5c5cb72b7eb1d3383276ff474706277378
IGutkowski <162903836+IGutkowski@users.noreply.github.com>
2024-12-17 16:06:18 +0100

    feat: svg for download instead of html sign

--- src/components/ProjectFiles/ProjectFiles.jsx
+++ src/components/ProjectFiles/ProjectFiles.jsx
@@ -37,11 +37,22 @@ const ProjectFiles = ({ project }) => {
                         <li key={fileId} className="file-item">
                             <span className="file-icon">📄</span>
                             <span className="file-name">{fileName}</span>
-                            <span
-                                className="download-text"
-                                onClick={() => handleDownload(fileId, fileName)}
-                            >
-                                ⬇
+                            <span className="download-text" onClick={() => handleDownload(fileId, fileName)}>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        width="24"
+                                        height="24"
+                                    >
+                                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                                    <polyline points="7 10 12 15 17 10" />
+                                    <line x1="12" y1="15" x2="12" y2="3" />
+                                  </svg>
                             </span>
                         </li>
                     ))}
